0c2f6bb2a4e0dd683245668dcae7724052e263df
Kunal-sharma24 <kunalsharma123450@gmail.com>
2026-08-28 13:35:46 +0530

    tree

---
+++ Binary_Tree/BT_representation.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left,right=NULL;
+    }
+};
+
+void inorder(Node* root){
+    if(root==NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+    if(root==NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root){
+    if(root==NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left= new Node(2);
+    root->right= new Node(3);
+    root->left->left= new Node(4);
+    root->left->right= new Node(5);
+    root->right->left= new Node(6);
+    root->right->right= new Node(7);
+
+    cout <<"Preoder traversal of binary tree is: \n";
+    preorder(root);
+    cout <<"\n" <<"Inorder traversal of binary tree is: \n";
+    inorder(root);
+    cout <<"\n" <<"Postorder traversal of binary tree is: \n";
+    postorder(root);
+
+    return 0;
+}
